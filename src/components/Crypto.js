e0a44e9fada86322aaf7d77b94dfaed43d2509bc
Brandon Ngo <okibrandon@hotmail.com>
2021-05-05 04:42:16 -0400

    Initial

---
+++ src/components/Crypto.js
@@ -0,0 +1,15 @@
+import React, { useState, useEffect } from "react";
+import { Container, Row, Col } from "react-bootstrap";
+
+function Crypto() {
+  const [] = useState();
+  
+  return (
+    <div>
+      <Container>
+
+
+      </Container>
+    </div>
+  );
+}
