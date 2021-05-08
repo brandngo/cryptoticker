2e4f2769863058d17d1185983c2774cba6d9f941
Brandon Ngo <okibrandon@hotmail.com>
2021-05-08 10:39:53 -0400

    added coinbase api

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from "./logo.svg";
 import "./App.css";
 import { Container, Row, Col } from "react-bootstrap";
+import Crypto from './components/Crypto.js';
 
 function App() {
   return (
     <div className="App">
+      <Crypto title="BTC"></Crypto>
       <Container>
         <Row>
           <Col></Col>
--- src/components/Crypto.js
+++ src/components/Crypto.js
@@ -1,15 +1,58 @@
 import React, { useState, useEffect } from "react";
-import { Container, Row, Col } from "react-bootstrap";
+import { Container, Row, Col, Button } from "react-bootstrap";
 
-function Crypto() {
-  const [] = useState();
-  
+function Crypto(props) {
+  const [value, setValue] = useState(0);
+
+  /*
+  useEffect(() => {
+    const apiUrl = 'https://api.coinbase.com/v2/exchange-rates?currency=' + props.title;
+    fetch(apiUrl)
+    .then((res) => res.json())
+    .then((data) => {
+      setValue(data.rates["USD"]);
+    });
+
+  }, []);
+
+  */
+
+  const title = props.title;
+  const findPrice = async (title) => {
+    const apiUrl = 'https://api.coinbase.com/v2/exchange-rates?currency=' + title;
+    let dataObj = [];
+    fetch(apiUrl)
+    .then((res) => res.json())
+    .then((data) => (dataObj = data));
+    //let data1 = JSON.parse(JSON.stringify(dataObj));
+    await setValue(dataObj.currency);
+    /*
+    if (dataObj.hasOwnProperty('rates')) {
+      console.log(dataObj.rates);
+
+    } else {
+      console.log("err");
+
+    }
+    */
+  }
+
+  //setInterval( , 1000);
+
   return (
     <div>
       <Container>
-
+        <Row>
+          <h1>{title}</h1>
+        </Row>
+        <Row>
+          <h2>{value}</h2>
+          <Button onClick={() => findPrice(title)}>Get Price of BTC</Button>
+        </Row>
 
       </Container>
     </div>
   );
 }
+
+export default Crypto;
